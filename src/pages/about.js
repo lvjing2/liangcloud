3759908bcb95e20eea9cc6b9c045e203b1ed5cbb
lvjing2 <leojames.googol@gmail.com>
2024-01-08 14:12:29 +0800

    using hugo

--- src/pages/about.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export default () =>
-    <div>
-        <h1>Hi! I'm building a site as a part of life!</h1>
-        <p> What do I like to do? Lots of course but definitely enjoy building website.</p>
-    </div>
